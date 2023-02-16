65dda749a92fbcf4d936bf2a701827e70a390fce
KamalLADIB <kamalladib98@gmail.com>
2023-02-16 13:19:37 -0800

    c lang 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /*this is the necessary comment*/
 int main(void) 
 {
-    puts("Programming is like building a multilingual puzzle");
-    puts("");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
