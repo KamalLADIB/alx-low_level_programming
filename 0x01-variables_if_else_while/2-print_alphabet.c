66b34b929836732cc99fa989ddb71a91420168e8
KamalLADIB <kamalladib98@gmail.com>
2023-02-18 12:49:30 -0800

    print aplphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (int i = 97; i < 123; i++)
+	putchar(i);
+return (0);
+}
