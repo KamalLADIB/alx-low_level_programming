a86051c83745199a8530016324d713d9b7cd17f5
KamalLADIB <kamalladib98@gmail.com>
2023-02-18 15:35:53 -0800

    all alphabet8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+	if (i < 9)
+	{
+	putchar(44);
+	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
