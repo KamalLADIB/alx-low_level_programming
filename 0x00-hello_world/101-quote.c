03ee978a38f8abfbbb9bbc389f6219ef37524ef9
KamalLADIB <kamalladib98@gmail.com>
2023-02-20 16:10:44 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char string[] = "\"and that piece of art is useful\"";
+char string1[] = " - Dora Korpar, 2015-10-19";
+int x;
+
+x = 0;
+while (string[x])
+{
+putchar(string[x]);
+x++;
+}
+x = 0;
+while (string1[x])
+{
+putchar(string1[x]);
+x++;
+}
+putchar('\n');
+return (1);
+}
