565337fb6f94aa6afdc4f2198b003ae9b821ea26
KamalLADIB <kamalladib98@gmail.com>
2023-02-21 20:28:50 -0800

    counting

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98  - Entry point
+ * @n: input number
+ *
+ * Return: sum of two numbers
+ *
+ */
+
+void print_to_98(int n);
+{
+int i;
+
+if (n <= 98)
+{
+printf("%d", n);
+
+for (i = n + 1; i < 99; i++)
+printf(", %d", i);
+}
+else
+{
+printf("%d", n);
+
+for (i = n - 1; i > 97; i--)
+printf(", %d", i);
+}
+}
