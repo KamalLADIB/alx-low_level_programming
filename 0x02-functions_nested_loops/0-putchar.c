dea402ffc3b3ca485bab82a0029c3f9ed348facd
KamalLADIB <kamalladib98@gmail.com>
2023-02-21 17:19:51 -0800

    first try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char message[] = "_putchar";
+int MAXSTRING = 100;
+
+for (int count = 0; count < MAXSTRING; count++)
+{
+if (message[count] == '\0')
+{
+putchar('\n');
+break;
+}
+else
+putchar(message[count]);
+}
+return (0);
+}
