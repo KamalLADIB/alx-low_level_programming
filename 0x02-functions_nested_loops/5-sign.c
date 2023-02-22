a87b87d81100d50320cb2383ebc8dbef6c63566f
KamalLADIB <kamalladib98@gmail.com>
2023-02-21 18:37:47 -0800

    signing

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign  - Entry point
+ * @n: single number input
+ *
+ * Return: 1 and prints + if n is greater than zero
+ * Return 0 and prints 0 if n is zero
+ * Return -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(46);
+return (-1);
+}
+return (0);
+}
