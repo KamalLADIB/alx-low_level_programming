30b44b5aa5a72398ae690705706407805f8b4230
KamalLADIB <kamalladib98@gmail.com>
2023-02-21 19:36:51 -0800

    federal agent1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 int a, b, c, d;
 for (a = 0; a < 3; a++)
 {
-for (b = 0; b < 4; b++)
+for (b = 0; (b < 4 && a == 2) || (b < 10 && a != 2); b++)
 {
 for (c = 0; c < 6; c++)
 {
