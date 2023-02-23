939929d43132e5134d0ced0458c5a2f241f5bf06
KamalLADIB <kamalladib98@gmail.com>
2023-02-22 16:40:33 -0800

    let's get natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_natural  - Entry point
+ * @n: input number
+ *
+ * Return: void
+ *
+ */
+
+void get_natural(int n)
+{
+if (n < 1024)
+{
+int sum, r, f, t, i;
+
+sum = 0;
+f = 0;
+t = 0;
+r = 5;
+while (r < n)
+{
+f++;
+r += 5;
+}
+
+r = 3;
+while (r < n)
+{
+t++;
+r += 3;
+}
+
+for (i = 1; i < f + 1; i++)
+sum += i * 5;
+
+for (i = 1; i < t + 1; i++)
+sum += i * 3;
+printf("%d\n", sum);
+}
+}
