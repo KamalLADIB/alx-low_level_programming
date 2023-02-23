c4ccba41520dd548356c38f5cd3692c05293cd27
KamalLADIB <kamalladib98@gmail.com>
2023-02-22 18:10:42 -0800

    even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long int a, b, c, d;
+
+i = 0;
+a = 1;
+b = 2;
+d = 2;
+c = 0;
+while (c < 4000000)
+{
+c = b + a;
+a = b;
+b = c;
+if (c % 2 == 0 && c < 4000000)
+d += c;
+}
+printf("%lu\n", d);
+return (0);
+}
