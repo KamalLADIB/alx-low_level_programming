15816c8ada23e7d1b5d684dfd7040bbf1371b8fa
KamalLADIB <kamalladib98@gmail.com>
2023-02-27 15:47:33 -0800

    is it a digit?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper  - Entry point
+ * @c: input number
+ *
+ * Return: sum of two numbers
+ *
+ */
+
+int _isupper(int c);
+{
+int i;
+
+i = 48;
+while (i < 58)
+{
+if (c == i)
+return (1);
+i++;
+}
+return (0);
+}
